9ea79e099c7be3214ed67eef8e5e083830b591e6
ohashiyukio <ohashiyukio222@gmail.com>
2024-10-25 09:09:34 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create,]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-     #@items = Item.all
+    @items = Item.order(created_at: :desc)
   end
 
   def new
