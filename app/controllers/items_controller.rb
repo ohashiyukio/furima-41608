e0e1761a523f3ecb77eca50a0df766002f7ffef5
ohashiyukio <ohashiyukio222@gmail.com>
2024-10-27 21:56:33 +0900

    削除機能訂正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy] # 未ログインユーザーはログインページにリダイレクト
-  before_action :set_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_item, only: [:edit, :update, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
