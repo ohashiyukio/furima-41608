75aa58a9314c659a6bf02a87b3bbe16e36feea54
ohashiyukio <ohashiyukio222@gmail.com>
2024-11-02 17:29:38 +0900

    connection変更

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,12 +1,5 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    before_action :basic_auth
-
-
-    private
-  
-    def basic_auth
-      authenticate_or_request_with_http_basic do |username, password|
-        username == 'admin' && password == '2222'
-      end
-    end
+    
+  end
+end
